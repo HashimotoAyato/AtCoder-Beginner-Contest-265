c259cdf77e6735bcf7dec2dfe391cfc4591b635b
HashimotoAyato <ayaenum@gmail.com>
2022-08-21 23:48:44 +0900

    add changes

--- d.py
+++ d.py
@@ -25,21 +25,10 @@ for l in range(N):
         else:
             break
     if R_SUM[r] + L_SUM[l] == target:
-        pairs.append([l+1, r])
+        pairs.append([l+2, r])
+
 
 for i in range(len(pairs)):
-    check1 = False
-    check2 = False
     sum = 0
-    for j in range(pairs[i][0], pairs[i][1]+1):
-        sum += A[j]
-        if check1 == False and sum == P:
-            check1 = True
-        
-        if check1 and sum == P+Q:
-            check2 = True
-            break
-    if check1 and check2:
-        break
+    check1 = check2 = check3 = False
     
-print('Yes' if check1 and check2 else 'No')
